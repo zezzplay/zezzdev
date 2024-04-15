aa84a0d55dc628abd0596e11c3d62859df5c4437
zezzplay <167049946+zezzplay@users.noreply.github.com>
2024-04-15 10:30:48 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,31 @@
+# Discord Bot Tutorial
+Series tutorial bot discord [YouTube](https://youtube.com/playlist?list=PLwDBhnWaq0eKYQEgfVaM2krsRBel2M11D)
+
+### Tutorial penggunaan
+
+- Clone repo
+```
+git clone https://github.com/ZTRdiamond/discord-bot-tutorial
+```
+
+- Install modules
+```js
+npm install
+```
+
+- Atur token di config.json atau .env
+
+Jika kamu mau menggunakan config.json untuk meletakan token, Kamu bisa ubah kode bagian token seperti dibawah
+```js
+token: config.token,
+```
+
+Dan jika kamu ingin menggunakan .env, Kamu bisa ubah kode bagian token seperti dibawah
+```js
+token: process.env.TOKEN,
+```
+
+***Note**: bila tidak mengerti jangan ubah-ubah terlalu banyak
+
+Join server discord [Zanixon Group™](https://ztrdiamond.my.to/invite.html) untuk support!
+
---
+++ index.js
@@ -0,0 +1,31 @@
+const aoijs = require("aoi.js");
+const dotenv = require('dotenv');
+const config = require('./config.json');
+dotenv.config()
+
+const { Util } = require("aoi.js");
+const { setup } = require("aoi.parser")
+setup(Util);
+
+const bot = new aoijs.AoiClient({
+token: config.token, //token
+database: {
+    db: require("aoi.db"),
+    type: "aoi.db",
+    path: "./database/",
+    tables: ["main"]},
+disableFunctions: ['$clientToken'],
+prefix: ['!'],//atur prefix sesuai pilihanmu
+intents: ["MessageContent","Guilds","GuildMessages","GuildMembers","GuildMessageReactions","GuildEmojisAndStickers","DirectMessageTyping","DirectMessageReactions","DirectMessages"],
+events: ["onMessage","onInteractionCreate","onFunctionError"]
+})
+
+bot.command({
+    name: "ping",
+    code: `
+🏓Pong! \`$pingms\`
+`//function $ping akan menampilkan speed latency dalam milidetik
+})
+
+const loader = new aoijs.LoadCommands(bot);
+loader.load(bot.cmd, "./commands/")
